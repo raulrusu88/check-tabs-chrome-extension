1609db890e90e183a710905cbcc16e71a7c4c256
R Raul <raul@hazdev.local>
2023-05-07 21:02:15 +0300

    refresh list on closed tab

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,16 @@ type TabType = {
   windowId: number;
 }
 
+/*
+  TODO:
+   1. [] Make it work for production
+   2. [X] Update the array when we close a tab
+   3. [] Take a default icon, that I can use for chrome:// tabs
+   4. [] Show if tab is playing audio or is muted
+   5. [] Check if there are duplicates and close one of them
+   6. [] Group e.g Youtube links, and with a drop down show every youtube tab open
+ */
+
 function App() {
   const [windows, setWindows] = useState<Record<string, any>>();
 
@@ -46,8 +56,13 @@ function App() {
   }
 
   const handleClose = useCallback((tabId: number) => {
+    setWindows((prevState) => {
+      const removeItem = windows?.tabs.filter((tab: TabType) => tab.id !== tabId);
+      return {...prevState, tabs: removeItem};
+    });
     // @ts-expect-error Exclude
     return chrome.tabs.remove(tabId)
+
   }, [windows]);
 
   useEffect(() => {
