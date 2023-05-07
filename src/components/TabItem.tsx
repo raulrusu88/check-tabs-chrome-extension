e259de4d011a344458e35731d497bbbc5f0561d2
R Raul <raul@hazdev.local>
2023-05-07 19:41:34 +0300

    changed string length to show on hover

--- src/components/TabItem.tsx
+++ src/components/TabItem.tsx
@@ -26,7 +26,7 @@ const TabItem = ({ favIconUrl, title, handleClose, url, active }: ITabItem) => {
         <div className={classes}>
             <div className="flex justify-start items-center">
                 <img src={favIconUrl} alt={title} className="w-8 h-8 mr-2"/>
-                <p title={title.length > 15 ? title : ""}>{truncate(title, 70)}</p>
+                <p title={title.length > 70 ? title : ""}>{truncate(title, 70)}</p>
             </div>
             <span className="cursor-pointer w-3 h-3 text-slate-100 hover:bg-red-700 rounded p-3 flex items-center justify-center" onClick={handleClose}>X</span>
         </div>
